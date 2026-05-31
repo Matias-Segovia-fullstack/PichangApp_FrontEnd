90df64d199223ec33d13c29e63e1631f07af081c
Luciano Machuca <luc.machuca@duocuc.cl>
2026-05-31 17:48:16 -0400

    fix conexion universal

--- Proyecto/pichangapp/lib/services/api_service.dart
+++ Proyecto/pichangapp/lib/services/api_service.dart
@@ -1,11 +1,20 @@
 import 'dart:convert';
 import 'dart:io';
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 
 class ApiService {
-  // Usamos 10.0.2.2 porque es el "localhost" del emulador de Android.
-  // Así el celular virtual se puede conectar al backend en tu computadora.
-  static const String baseUrl = 'http://10.0.2.2:8001/api';
+  // Detecta automáticamente en qué entorno estás ejecutando la app y asigna la IP correcta.
+  // Así ningún colaborador tendrá problemas si usa Web, Android, Windows o iOS.
+  static String get baseUrl {
+    if (kIsWeb) {
+      return 'http://127.0.0.1:8001/api'; // Chrome / Web
+    } else if (defaultTargetPlatform == TargetPlatform.android) {
+      return 'http://10.0.2.2:8001/api';  // Emulador Android
+    } else {
+      return 'http://127.0.0.1:8001/api'; // Windows / iOS / Mac
+    }
+  }
 
   /// Realiza la petición POST al backend para registrar un nuevo usuario.
   /// Retorna [true] si la cuenta se creó exitosamente (código 201 o 200).
