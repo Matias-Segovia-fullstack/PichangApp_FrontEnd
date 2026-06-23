5c1d50c1f366eafd94988472a4a1070b2af9edbb
Matias <mati.segovia@duocuc.cl>
2026-06-23 01:01:32 -0400

    fix test pantalla de inicio

--- Proyecto/pichangapp/test/widget_test.dart
+++ Proyecto/pichangapp/test/widget_test.dart
@@ -1,11 +1,17 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:pichangapp/main.dart';
 
 void main() {
   testWidgets('La app carga sin errores', (WidgetTester tester) async {
     await tester.pumpWidget(const PichangApp());
-    await tester.pump();
 
-    expect(find.text('PichangApp'), findsOneWidget);
+    await tester.pump(const Duration(seconds: 1));
+    await tester.pump(const Duration(seconds: 1));
+
+    expect(find.byType(MaterialApp), findsOneWidget); 
   });
+
+
+  
 }
